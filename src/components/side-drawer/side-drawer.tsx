949fd08fbe028dde663f69ee58493e32f0a82c16
Leticia Lopes <leticialopes236@gmail.com>
2023-04-29 23:07:35 -0300

    botão de abrir e fechar o menu

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -8,22 +8,27 @@ import { Component, h, Prop } from '@stencil/core';
 
 export class SideDrawer { 
   @Prop({ reflect: true }) title: string; // agora o Stencil vai observar atributos com o nome de title no nosso componente e se setarmos ou mudarmos como um atributo, ou se setarmos diretamente e programaticamente o título de fora do javascript, então o Stencil vai detectar essa mudaça e isso vai automaticamente reexecutar o método render de uma maneira muito eficiente, então não recarregará toda a DOM
-  @Prop() open: boolean;
+  @Prop({ reflect: true, mutable: true }) open: boolean; // @Props definidas aqui são imutáveis, não podemos mudar dentro do componente. No html ou manualmente podemos, a não ser que coloquemos 'mutable: true'
+
+  onCloseDrawer() {
+    this.open = false;
+  }
 
   render () { 
-    let content = null;
-    if (this.open) {
-      content = (
-        <aside>
+    return (
+      <aside>
         <header>
           <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>         
         </header>
+        <section id='tabs'>
+          <button class='active'>Navigation</button>
+          <button>Contact</button>
+        </section>
         <main>
           <slot></slot>
         </main>
       </aside>
-      );
-    }
-    return content;
+    )
   }
 }
