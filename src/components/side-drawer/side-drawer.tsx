27ac13f2c929d5b2fac5d6df8601510df68d9142
Leticia Lopes <leticialopes236@gmail.com>
2023-04-30 16:38:43 -0300

    more event listener and state uses

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Prop, State } from '@stencil/core';
 
 @Component({ 
   tag: 'wj-side-drawer', 
@@ -7,6 +7,7 @@ import { Component, h, Prop } from '@stencil/core';
 }) 
 
 export class SideDrawer { 
+  @State() showContactInfo = false;
   @Prop({ reflect: true }) title: string; // agora o Stencil vai observar atributos com o nome de title no nosso componente e se setarmos ou mudarmos como um atributo, ou se setarmos diretamente e programaticamente o título de fora do javascript, então o Stencil vai detectar essa mudaça e isso vai automaticamente reexecutar o método render de uma maneira muito eficiente, então não recarregará toda a DOM
   @Prop({ reflect: true, mutable: true }) open: boolean; // @Props definidas aqui são imutáveis, não podemos mudar dentro do componente. No html ou manualmente podemos, a não ser que coloquemos 'mutable: true'
 
@@ -14,7 +15,30 @@ export class SideDrawer {
     this.open = false;
   }
 
+  onContentChange(content: string) {
+    this.showContactInfo = content === 'contact' ? true : false; /* quero checar que o content é igual a contato -> se o conteúdo for igual a contato, e o resultado será true [= não é =, = você atribui, === está comparando] */ 
+  }
+
   render () { 
+    // para sermos capazes de 'ouvir' os cliques nesses botões e mudar o conteúdo que aparece no slot do main através do clique
+    let mainContent = <slot />;
+
+    if(this.showContactInfo) {
+      mainContent = (
+        <div id='contact-information'>
+          <h2>Contact Information</h2>
+          <p>You can reach us via phone or email</p>
+          <ul>
+            <li>Phone: 4898564451</li>
+            <li>
+              E-mail: {' '}
+              <a href="mailto:something@something.com">something@something.com</a>
+            </li>
+          </ul>
+        </div>
+      )
+    }
+    
     return (
       <aside>
         <header>
@@ -22,11 +46,11 @@ export class SideDrawer {
           <button onClick={this.onCloseDrawer.bind(this)}>X</button>         
         </header>
         <section id='tabs'>
-          <button class='active'>Navigation</button>
-          <button>Contact</button>
+          <button class={!this.showContactInfo ? 'active' : ''} /* se for false -> coloca ativo */ onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
+          <button class={this.showContactInfo ? 'active' : ''} /* se for true -> coloca ativo */ onClick={this.onContentChange.bind(this, 'contact')}>Contact</button> 
         </section>
         <main>
-          <slot></slot>
+          {mainContent}
         </main>
       </aside>
     )
